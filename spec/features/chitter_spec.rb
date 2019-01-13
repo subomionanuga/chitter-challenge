ac25c55cf30b197e8f313b92b466be2761eb28d2
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-13 22:33:24 +0000

    Post peep feature test passing

--- spec/features/chitter_spec.rb
+++ spec/features/chitter_spec.rb
@@ -6,7 +6,7 @@ feature 'Homepage' do
 
   scenario 'Able to post messages to chitter' do
     visit '/'
-    fill_in "peep", with: "This is my first peep"
+    fill_in "message", with: "This is my first peep"
     click_button "Post peep"
     expect(page).to have_content "This is my first peep"
   end
