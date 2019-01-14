fdf8a4cd7ccb684bd918933b97951133ff1c30de
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-14 01:43:20 +0000

    Not yet redirecting to profile page after sign up

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class Chitter < Sinatra::Base
   end
 
   post '/peep' do
-    @peep = Peep.create(message: params[:message])
+    @peep = Peep.create(message: params[:message]) #user_id: params[:id]#
     redirect '/'
   end
 
@@ -24,6 +24,12 @@ class Chitter < Sinatra::Base
   post '/signup' do
     user = User.create(email: params[:email], password: params[:password], name: params[:name], username: params[:username])
     session[:id] = user.id
-    redirect "/profile"
+    redirect "/profile/#{session[:id]}"
+  end
+
+  get '/profile/:id' do
+    @user = User.get(params[:id])
+    # @peeps = Peep.all(:conditions => { :user_id => params[:id] })
+    erb(:profile)
   end
 end
